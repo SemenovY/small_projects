5916e06d8c7f404dc631c5a744a92a16f697f94a
Semenov Yuriy <info@juriys.ru>
2024-01-31 17:30:40 +0300

    add file

--- mini_games/tprogger.py
+++ mini_games/tprogger.py
@@ -1,34 +1,11 @@
-# product = 'y'
-# while product == 'y':
-#     product1 = int(input("Enter1: "))
-#     product2= int(input("Enter2: "))
-#     print(product1 + product2)
-#     product = input('Repeat?: ')
-# print([i for i in range(0, 1000, 10)])
+def main():
+    outfile = open("tprogged.txt", "w")
+    outfile.write("test1\n")
+    outfile.write("test2\n")
+    outfile.write("test3\n")
 
-# sum = 0
-# for i in range(10):
-#     num = int(input('Enter a number: '))
-#     sum += num
-# print(sum)
+    outfile.close()
 
-# res = 0
-# for k in range(1, 31):
-#     res += k/(31 - k)
-# print(res)
 
-# for i in range(10):
-#     for j in range(15):
-#         print('*', end='')
-#     print()
-
-# result = 0
-# for i in range(5):
-#     res = int(input('Enter a number: '))
-#     result += res
-# print(result)
-
-call = 4.2
-print("------------------------")
-for i in range(10, 31, 5):
-    print(f"{i}\t{i*call}")
+if __name__ == "__main__":
+    main()
